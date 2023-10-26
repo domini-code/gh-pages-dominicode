b34dc1400870adf3fd9ad5688f415d73b08c1726
bezael <bezael@gmail.com>
2023-10-26 23:39:47 +0200

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={solidLogo} class='logo solid' alt='Solid logo' />
         </a>
       </div>
-      <h1>DOMINICODE v2</h1>
+      <h1>GITHUB PRESENT</h1>
       <div class='card'>
         <button onClick={() => setCount((count) => count + 1)}>count is {count()}</button>
         <p>
